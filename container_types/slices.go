eae0f4f69d654c5d04b3f839b09d8feee0f36506
Pedro Lopez <pedrolopz.85@gmail.com>
2023-06-10 09:55:36 +0200

    slices

---
+++ container_types/slices.go
@@ -0,0 +1,70 @@
+/*
+Los slices son una abastracción de los arrays de GO
+A diferencia de los arrays, en la definiciòn de los slices
+no se incluye su longitud
+
+Un array sin inicializar, es nil y su len=0
+Si queremos inicializar un array vacio y con
+determinada longitud, tenemos que user el método make
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	/*
+		Un array sin inicializar, es nil y su len=0
+		Si queremos inicializar un array vacio y con
+		determinada longitud, tenemos que user el método make
+	*/
+	var s []string
+	fmt.Println("slice s:", s, s == nil, len(s) == 0)
+
+	/*
+		Si queremos inicializar un array previamente vacio y con
+		determinada longitud, tenemos que user el método make
+	*/
+
+	s = make([]string, 3)
+	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	fmt.Println("len:", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+}
